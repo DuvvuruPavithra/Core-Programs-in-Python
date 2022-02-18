5ee1f9ddd8389145f5fc8c2dd40703fb06dcb3ff
DuvvuruPavithra <duvvurupavithra28@gmail.com>
2022-02-18 10:37:42 +0530

    Added: Take the input from User and print the table of powers of two is added successfully

---
+++ Power_of_two.py
@@ -0,0 +1,13 @@
+def power_of_two():
+    #taking the input from the user
+    num = int(input("Enter a number: "))
+
+    #using for loop to finding power of two
+   for i in range(1 , num + 1):
+       print("2 * ",i,"=",2**i)
+power_of_two()
+
+
+
+
+
